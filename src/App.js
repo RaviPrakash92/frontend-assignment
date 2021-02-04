78efdbe1e87405579e6f885cce3fc63cf10d6a02
Ravi Prakash <ravi2010prakash@gmail.com>
2021-02-04 20:57:46 +0530

    Commit Successfull.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import  Gallery  from "./Modules/Gallery";
+
+export default class Apps extends Component{
+  render(){
+    return (
+      <div>
+        <Gallery/>
+      </div>
+    )
+  }
+}
